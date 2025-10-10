92c258e139b780e96780af81e6fccaca7c6a0c74
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-10-10 16:18:27 +0530

    push

--- auth.config.ts
+++ auth.config.ts
@@ -5,14 +5,14 @@ import type { NextAuthConfig } from "next-auth"
 
 
 export default {
-    ptovider: [
-        Github({
-            clientId: process.env.GITHUB_ID,
-            clientSecret :process.env.GITHUB_SECRET
-        }),
-        Google({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret :process.env.GOOGLE_SECRET
-        })
-]
-}
+  providers: [
+    Github({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+};
--- auth.ts
+++ auth.ts
@@ -1,4 +1,11 @@
-import NextAuth from 'next-auth'
-import { PrismaAdapter } from "@auth/prisma-adapter"
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "./lib/db";
+import authConfig from "./auth.config";
 
-export const {auth , handlers , signIn , signOut} = NextAuth({})
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  secret: process.env.AUTH_SECRET,
+  ...authConfig,
+});
