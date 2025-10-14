e7ac7d499294555f734b9e23d9702af0273c3e4a
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-10-14 15:30:32 +0530

    updated

--- middleware.ts
+++ middleware.ts
@@ -8,5 +8,12 @@ const { auth } = NextAuth(authConfig)
 export default auth((req) => {
     const { nextUrl } = req
     const isLoggedIn = !!req.auth;
-    return isLoggedIn ? nextUrl : '/login'
-})
+
+})    
+
+
+export const config = {
+    // copid form clerk
+    
+    matcher: [ "/((?!api|_next/static|_next/image|favicon.ico).*)"],
+}
