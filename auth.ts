9d198881ba620958ab53d7cdda5a2b52cc052b7e
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-10-15 09:44:48 +0530

    updated

--- auth.ts
+++ auth.ts
@@ -2,8 +2,48 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "./lib/db";
 import authConfig from "./auth.config";
+import { create } from "domain";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      if (!user || !account) return false;
+
+      const existingUser = await db.user.findUnique({
+        where:{email : user.email}
+      })
+
+      if (!existingUser) { 
+        const newUser = await db.user.create({
+          data: {
+            email: user.email!,
+            name: user.name,
+            image : user.image,
+            account: {
+              create: {
+                type: account.type,
+                provider: account.provider,
+                providerAccountId: account.providerAccountId,
+                refreshToken: account.refresh_token,
+                access_token: account.access_token,
+                expiresAt: account.expires_at,
+                tokenType: account.token_type,
+                scope: account.scope,
+                isToken: account.is_token,
+                sessionState: account.session_state 
+
+              }
+            }
+          }
+        })
+
+        if (!newUser) return false;
+        
+      }
+     }
+   }, 
+
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   secret: process.env.AUTH_SECRET,
--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,40 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
+import { apiAuthPrefix, DEFAULT_LOGIN_REDIRECT, publicRoutes } from "./routes";
 
-
-
-const { auth } = NextAuth(authConfig)
+const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
-    const { nextUrl } = req
-    const isLoggedIn = !!req.auth;
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
 
-})    
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
 
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return null;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT));
+    }
+
+    return null;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/auth/sign-in", nextUrl));
+  }
+
+  return null;
+});
 
 export const config = {
-    // copid form clerk
-    
-    matcher: [ "/((?!api|_next/static|_next/image|favicon.ico).*)"],
-}
+  // copid form clerk
+
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
